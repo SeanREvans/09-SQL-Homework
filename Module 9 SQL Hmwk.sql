12fae62c4feb930054dc41a4e52ee0d7e1dc6c51
Sean Evans <seanrevans91@gmail.com>
2019-04-28 18:42:49 -0400

    Final

--- Module 9 SQL Hmwk.sql
+++ Module 9 SQL Hmwk.sql
@@ -119,10 +119,104 @@ AND fa.film_id = (
 	FROM film
 	WHERE title = "Alone Trip");
 
+#7c You want to run an email marketing campaign in Canada, 
+#for which you will need the names and email addresses of all Canadian customers. 
+#Use joins to retrieve this information. Country, customer, address
+SELECT c.first_name, c.last_name, c.email
+FROM customer c
+WHERE address_id in (
+	SELECT a.address_id
+	FROM address a, city c, country cn
+	WHERE c.city_id = a.city_id
+	AND c.country_id = cn.country_id
+    AND cn.country = "Canada"
+);
+
+#7d. Sales have been lagging among young families, 
+#and you wish to target all family movies for a promotion. 
+#Identify all movies categorized as family films.
+
+SELECT f.film, f.description AS "Family Films"
+FROM film f
+WHERE f.film_id in (
+	SELECT fc.film_id
+    FROM film_category fc, category c
+    WHERE fc.category = c.category_id
+    AND c.name = "Family"
+);
+
+#7e Display the most frequently rented movies in descending order.
+SELECT i.film_id, f.title, COUNT(r.inventory_id) AS "Rentals"
+FROM inventory i, film f, rental r
+WHERE r.inventory_id = i.inventory_id
+AND i.film_id = f.film_id
+GROUP BY i.film_id, f.title
+ORDER BY Rentals DESC;
+
+#7f. Write a query to display how much business, in dollars, each store brought in.
+SELECT c.store_id, SUM(p.amount) AS "Total Business"
+FROM customer c, payment p
+WHERE c.customer_id = p.customer_id
+GROUP BY c.store_id;
+
+#7g. Write a query to display for each store its store ID, city, and country.
+SELECT s.store_id, c.city, cr.country
+FROM country cr, store s, city c, address a
+WHERE s.address_id = a.address_id 
+AND  a.city_id = c.city_id
+AND c.country_id = cr.country_id;
+
+
+#7h List the top five genres in gross revenue in descending order. 
+#(Hint: you may need to use the following tables: category, film_category, inventory, payment, and rental.)
+SELECT c.name, SUM(pf.amount) AS "Gross Revenue"
+FROM
+	(SELECT pi.amount, fc.category_id
+	FROM 
+		(SELECT pr.amount, i.film_id
+		FROM
+			(SELECT p.amount, r.inventory_id
+			FROM payment p 
+			INNER JOIN rental r
+			ON p.rental_id = r.rental_id) AS pr
+		INNER JOIN inventory i
+		ON pr.inventory_id = i.inventory_id) AS pi
+	INNER JOIN film_category fc
+	ON pi.film_id = fc.film_id) AS pf
+INNER JOIN category c
+ON pf.category_id = c.category_id
+GROUP BY c.name 
+ORDER BY "Gross Revenue" DESC;
+
+#8a. In your new role as an executive, you would like to have an easy way of viewing the Top five genres by gross revenue. 
+#Use the solution from the problem above to create a view. 
+#If you haven't solved 7h, you can substitute another query to create a view.
+
+CREATE VIEW `Film Gross Revenue` AS
+SELECT c.name, SUM(pf.amount) AS "Gross Revenue"
+FROM
+	(SELECT pi.amount, fc.category_id
+	FROM 
+		(SELECT pr.amount, i.film_id
+		FROM
+			(SELECT p.amount, r.inventory_id
+			FROM payment p 
+			INNER JOIN rental r
+			ON p.rental_id = r.rental_id) AS pr
+		INNER JOIN inventory i
+		ON pr.inventory_id = i.inventory_id) AS pi
+	INNER JOIN film_category fc
+	ON pi.film_id = fc.film_id) AS pf
+INNER JOIN category c
+ON pf.category_id = c.category_id
+GROUP BY c.name 
+ORDER BY "Gross Revenue" DESC;
 
 
 
+#8b. How would you display the view that you created in 8a?
+SELECT * FROM `film gross revenue`;
 
-;
-
- 
+#8c. You find that you no longer need the view top_five_genres. 
+#Write a query to delete it.
+DROP VIEW `film gross revenue`;
